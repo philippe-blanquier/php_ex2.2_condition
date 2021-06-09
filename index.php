29e6481accd83aeabc67f323d4b9b51dbaac62c8
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 12:07:19 +0200

    evo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex2.2 condition</title>
+    </head>
+    <body >
+        <?php
+        $isEasy=TRUE;
+        if ($isEasy)
+        {
+            echo ("C'est facile");
+        }
+        else
+        {
+            echo ("C'est difficile");
+        }
+       ?>
+    </body>
+</html>
